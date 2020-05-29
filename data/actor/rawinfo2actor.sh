097218f9a67fd4bd68c4844c9153b0f246d5b035
Absnormal <absnormal26065873@gmail.com>
2020-05-30 01:03:08 +0800

    actor table done

--- data/actor/rawinfo2actor.sh
+++ data/actor/rawinfo2actor.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # parameters
-INF=$1
-TMP=tmp_$1
-ACT=$2
+PWD=`pwd`
+INF=$PWD/$1
+TMP=$PWD/$1.tmp
+ACT=$PWD/$2
 
 # detect parameters
 if [ -z "$2" ]; then
@@ -20,30 +21,37 @@ fi
 #   主要演員：克里斯多福諾蘭,克里斯汀貝爾,米高肯恩,蓋瑞歐德曼,凱蒂荷姆絲,摩根費里曼,連恩尼遜
 #   影片類型：動作
 # ->
-#   克里斯多福諾蘭,,,,蝙蝠俠：開戰時刻;...
-#   克里斯汀貝爾,,,,蝙蝠俠：開戰時刻;...
-#   米高肯恩,,,,蝙蝠俠：開戰時刻;...
-#   蓋瑞歐德曼,,,,蝙蝠俠：開戰時刻;...
-#   凱蒂荷姆絲,,,,蝙蝠俠：開戰時刻;...
-#   摩根費里曼,,,,蝙蝠俠：開戰時刻;...
-#   連恩尼遜,,,,蝙蝠俠：開戰時刻;...
+#   克里斯多福諾蘭,,,
+#   克里斯汀貝爾,,,
+#   米高肯恩,,,
+#   蓋瑞歐德曼,,,
+#   凱蒂荷姆絲,,,
+#   摩根費里曼,,,
+#   連恩尼遜,,,
 
 # generate a temp csv
 cp $INF $TMP
 
 # remove unnessarys
+`sed -i 's/\s//g' $TMP`
+`sed -i '/title/ d' $TMP`
 `sed -i '/EN_title/ d' $TMP`
 `sed -i '/intro/ d' $TMP`
-`sed -i '/影片類型/ d' $TMP`
+`sed -i 's/影片類型.*/,/g' $TMP`
 `sed -i 's/主要演員：//g' $TMP`
 `sed -i '/---/ d' $TMP`
 `sed -i 's/\.//g' $TMP`
 
-# separate actors by line
-`sed -i 's/,/\n/g' $TMP`
+# fix
+`sed -i ':a;N;$!ba; s/\n//g' $TMP`
+`sed -i 's/,\+/,/g' $TMP`
+`sed -i 's/,/,,,\n/g' $TMP`
+`sed -i '$ d' $TMP`
 
 # generate csv file and add attribute name to first line
 `touch $ACT`
-`echo "name,SSN,sex,birth,movies" > $ACT`
+`echo "name,SSN,sex,birth" > $ACT`
 `cat $TMP >> $ACT`
+# sort and uniq
+#`sort $TMP | uniq >> $ACT`
 rm $TMP
