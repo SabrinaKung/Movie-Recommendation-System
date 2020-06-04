fcafd555e1d7c4cdba45f3936f8738937ca57cc9
Absnormal <absnormal26065873@gmail.com>
2020-06-04 15:29:52 +0800

    mac_config.sh prevent double config

--- mac_config.sh
+++ mac_config.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 PWD=`pwd`
 
+# remove additional parameter for sed -i (if have)
+sed -i '' "s/-i \'\'/-i/g" $PWD/data/actor/rawinfo2actor.sh
+sed -i '' "s/-i \'\'/-i/g" $PWD/data/acts/rawinfo2act.sh
+sed -i '' "s/-i \'\'/-i/g" $PWD/data/display/display_txt2csv.sh
+sed -i '' "s/-i \'\'/-i/g" $PWD/data/movie/rawdisplay2movie.sh
+sed -i '' "s/-i \'\'/-i/g" $PWD/data/type/rawinfo2type.sh
+
 # add additional parameter for sed -i
 sed -i '' "s/-i/& \'\'/g" $PWD/data/actor/rawinfo2actor.sh
 sed -i '' "s/-i/& \'\'/g" $PWD/data/acts/rawinfo2act.sh
